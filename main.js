f46a4f5716a5dce874c77e243361ca3861147827
Kellen <kellenbruce@outlook.com>
2019-10-15 22:29:07 -0700

    Fixed main

--- main.js
+++ main.js
@@ -2,4 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("mainHeader").onclick = function() {
       this.style.color = 'green'
     }
+    $( document ).ready(function() {
+      $( "#catPhoto" ).click(function() {
+        $( "#catPhoto" ).fadeOut( "slow", function() {
+        });
+      });
+    });
   })
+  
